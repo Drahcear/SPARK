baec3cc15add2ac0f447dc6bf76fe6fcd35758ee
richard.lay <richard.lay@epita.fr>
2022-05-25 08:02:09 +0200

    update format producer to avro

--- implem/drone/build.sbt
+++ implem/drone/build.sbt
@@ -5,10 +5,11 @@ ThisBuild / scalaVersion := "2.13.8"
 libraryDependencies += "org.apache.kafka" %% "kafka" % "3.2.0"
 libraryDependencies += "org.apache.kafka" % "kafka-streams" % "3.2.0"
 libraryDependencies += "org.apache.kafka" %% "kafka-streams-scala" % "3.2.0"
+libraryDependencies += "io.confluent" % "kafka-avro-serializer" % "7.1.0"
 
 libraryDependencies += "net.liftweb" %% "lift-json" % "3.5.0"
 libraryDependencies += "com.google.code.gson" % "gson" % "2.7"
-
+resolvers += "confluent" at "https://packages.confluent.io/maven/"
 
 lazy val root = (project in file("."))
   .settings(
--- implem/drone/src/main/scala/MessageUtils.scala
+++ implem/drone/src/main/scala/MessageUtils.scala
@@ -1,14 +1,10 @@
 import java.util.Properties
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
-import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
-import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.consumer.ConsumerRecords
-import org.apache.kafka.clients.producer.RecordMetadata
 import com.google.gson._
-
-import java.time.Duration
-import scala.collection.JavaConverters._
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericData
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord, RecordMetadata}
 
 object MessageUtils {
 
@@ -32,31 +28,54 @@ object MessageUtils {
     lines.map(line => gson.fromJson(line, classOf[Citizen])).toList
   }
 
-  def main(args: Array[String]): Unit = {
-    val topicName = "topic1"
 
-    val props: Properties = new Properties()
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
-    val producer: KafkaProducer[String, String] = new KafkaProducer[String,String](props)
-    val key = "test1"
-    val value = "Message 1"
-    val record = new ProducerRecord[String, String](topicName, key, value)
-    producer.send(record, (recordMetadata: RecordMetadata, exception: Exception) => {
-      if (exception != null){
-        exception.printStackTrace()
-      }
-      else {
-        println(recordMetadata)
-      }
-    })
-    producer.send(new ProducerRecord[String, String]("topic1", "test2", "Message 2"))
-    producer.send(new ProducerRecord[String, String]("topic2", "test3", "Affiche toi"))
-
-    producer.close()
-
-    Consumer.poll()
+  def createCitizenRecord(name: String, firstName : String, peacescore : Int, schema : String) : GenericRecord = {
+    val citizenSchema : Schema = new Schema.Parser().parse(schema)
+    val record = new GenericData.Record(citizenSchema)
+    record.put("Name", name)
+    record.put("FirstName", firstName)
+    record.put("PeaceScore", peacescore)
+    record
   }
 
+  def main(args: Array[String]): Unit = {
+    val props = new Properties()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+      classOf[KafkaAvroSerializer].getCanonicalName)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+      classOf[KafkaAvroSerializer].getCanonicalName)
+    props.put("schema.registry.url", "http://localhost:8081")
+    val producer = new KafkaProducer[String, GenericRecord](props)
+    val key = "key1"
+
+    val userSchema = "{\n  \"name\": \"MyClass\",\n  \"type\": \"record\",\n  \"namespace\": \"com.acme.avro\",\n  \"fields\": [\n    {\n      \"name\": \"id\",\n      \"type\": \"string\"\n    },\n    {\n      \"name\": \"location\",\n      \"type\": \"string\"\n    },\n    {\n      \"name\": \"Date\",\n      \"type\": \"int\"\n    },\n    {\n      \"name\": \"Citizens\",\n      \"type\": {\n        \"type\": \"array\",\n        \"items\": {\n          \"name\": \"Citizens_record\",\n          \"type\": \"record\",\n          \"fields\": [\n            {\n              \"name\": \"Name\",\n              \"type\": \"string\"\n            },\n            {\n              \"name\": \"FirstName\",\n              \"type\": \"string\"\n            },\n            {\n              \"name\": \"Login\",\n              \"type\": \"string\"\n            },\n            {\n              \"name\": \"PeaceScore\",\n              \"type\": \"int\"\n            }\n          ]\n        }\n      }\n    },\n    {\n      \"name\": \"Words\",\n      \"type\": {\n        \"type\": \"array\",\n        \"items\": \"string\"\n      }\n    }\n  ]\n}"
+    val citizenString = "{\n  \"name\": \"MyClass\",\n  \"type\": \"record\",\n  \"namespace\": \"com.acme.avro\",\n  \"fields\": [\n    {\n      \"name\": \"Name\",\n      \"type\": \"string\"\n    },\n    {\n      \"name\": \"FirstName\",\n      \"type\": \"string\"\n    },\n    {\n      \"name\": \"Login\",\n      \"type\": \"string\"\n    },\n    {\n      \"name\": \"PeaceScore\",\n      \"type\": \"int\"\n    }\n  ]\n}"
+
+    val schema: Schema = new Schema.Parser().parse(userSchema)
+    val words = List("ah", "bon", "maintenant", "c'est", "comme", "ca")
+
+    val avroRecord = new GenericData.Record(schema)
+    val listUser = List(("Grand", "Jacky", 100), ("Grand","Steven",100), ("Grand", "Richard", 100))
+    val avroCitizens = listUser.map(x => createCitizenRecord(x._1,x._2, x._3, citizenString))
+
+    avroRecord.put("id", 667)
+    avroRecord.put("location", "dzdzdzedzd")
+    avroRecord.put("Date", 21212)
+    avroRecord.put("Citizens", avroCitizens)
+    avroRecord.put("Words", words)
+
+    //val writer = new SpecificDatumWriter[GenericRecord](schema)
+    //val out = new ByteArrayOutputStream()
+    //val encoder: BinaryEncoder = EncoderFactory.get().binaryEncoder(out, null)
+    //writer.write(avroRecord, encoder)
+    //encoder.flush()
+    //out.close()
+    //val serializedBytes: Array[Byte] = out.toByteArray()
+    val record = new ProducerRecord[String, GenericRecord]("b", key, avroRecord)
+
+    producer.send(record)
+    producer.flush()
+    producer.close()
+  }
 }

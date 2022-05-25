90ab1a67710bf3aaa6a147c4ba2f5a3ccad83223
richard.lay <richard.lay@epita.fr>
2022-05-25 15:49:41 +0200

    récup la donnée depuis azure avec spark pour le mettre dans un df sur un schéma avro simple

--- implem/data_processing/build.sbt
+++ implem/data_processing/build.sbt
@@ -2,6 +2,13 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.8"
 
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.1" % "provided"
+libraryDependencies += "org.apache.hadoop" % "hadoop-azure" % "3.3.3"
+libraryDependencies += "org.apache.spark" %% "spark-avro" % "3.2.1"
+resolvers += "confluent" at "https://packages.confluent.io/maven/"
+libraryDependencies += "io.confluent" % "kafka-avro-serializer" % "7.1.0"
+
 lazy val root = (project in file("."))
   .settings(
     name := "data_processing"
--- implem/data_processing/src/main/scala/Main.scala
+++ implem/data_processing/src/main/scala/Main.scala
@@ -1,5 +1,29 @@
+import org.apache.avro.Schema
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val input_blob_path = "topics/b/partition=0/"
+    val output_blob_path = "topics/test/new"
+    val storageAccountName = "peaceland"
+    val storageKeyValue = "T+Q1Ryvhx2MtghOSccN9oUeQwRM4I9GwiBy6L/6K2r2i98WkADBBa+KEA/t6aJO6Ii7nJU6+p4yPSgJppnG9YQ=="
+    val containerName = "data"
+    val sparkSession = SparkSession.builder()
+      .master("local")
+      .appName("test")
+      .config(
+        s"fs.azure.account.key.${storageAccountName}.blob.core.windows.net",
+        storageKeyValue)
+      .getOrCreate()
+
+    val schemaString = "{\"type\":\"record\"," +
+      "\"name\":\"myrecord\"," +
+      "\"fields\":[{\"name\":\"f1\",\"type\":\"string\"}]}"
+    val schema: Schema = new Schema.Parser().parse(schemaString)
+    val df = sparkSession.read
+      .option("avroSchema", schema.toString)
+      .format("avro")
+      .load(s"wasbs://${containerName}@${storageAccountName}.blob.core.windows.net/${input_blob_path}")
+    df.show()
   }
 }

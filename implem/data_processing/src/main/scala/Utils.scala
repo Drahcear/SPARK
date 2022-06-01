425109e67b554220cf9835cad03c7beb18c38f16
Steven Tien <steven.tien@epita.fr>
2022-06-01 18:31:08 +0200

    Update data lake path

--- implem/data_processing/src/main/scala/Utils.scala
+++ implem/data_processing/src/main/scala/Utils.scala
@@ -2,8 +2,13 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.{array, asc, col, date_format, desc, explode, from_unixtime, lit, udf}
 
 object Utils {
+  case class Citizen(
+                      Name : String,
+                      FirstName : String,
+                      Login : String,
+                      PeaceScore : Int
+                    )
   def wordCount(df: DataFrame): Unit = {
-    //val badWords = List("students", "is", "for")
     println("Top 20 most used words :")
     val list = df.select("Words")
       .collect()
@@ -12,36 +17,39 @@ object Utils {
       .groupMapReduce(_._1)(_ => 1)(_+_)
       .toSeq.sortWith(_._2 > _._2)
       .take(20)
-      //.filter(x => badWords.contains(x._1))
       .foreach(x => println(x))
   }
 
   def getScore(df: DataFrame): DataFrame = {
-    val tuple_udf = udf((l:Seq[String]) => l.map(x => (x, 1)))   // Give a value to the word
-    val df2 = df.withColumn("Score", tuple_udf(col("Words")))
-    val score_udf = udf((l:Seq[(String,Integer)]) => l.reduce((x, y) => ("score" ,x._2 + y._2))._2)  // Get de score
-    val df3 = df2.withColumn("Score", score_udf(col("Score")))
-    df3
+    val length_udf = udf((l:Seq[Citizen]) => l.map(x => (x, 1)))   // Give a value to the word
+    val df2 = df.withColumn("Length", length_udf(col("Citizens")))
+    val reduce_udf = udf((l:Seq[(Citizen,Integer)]) => l.reduce((x, y) => (x._1 ,x._2 + y._2))._2)  // Get de score
+    val df3 = df2.withColumn("Length", reduce_udf(col("Length")))
+    val tuple_udf = udf((l:Seq[Citizen]) => l.map(x => (x.Login, x.PeaceScore)))
+    val df4 = df3.withColumn("PeaceScore", tuple_udf(col("Citizens")))
+    val score_udf2 = udf((l:Seq[(String, Integer)]) => l.reduce((x, y) => ("Score", x._2 + y._2))._2)
+    val df5 = df4.withColumn("PeaceScore", score_udf2(col("PeaceScore"))/col("Length"))
+    df5
   }
 
   def getLowestScore(df: DataFrame): Unit = {
     val df1 = getScore(df)
-    df1.orderBy(asc("Score"))
-      .show(1)   // Get the row with the lowest score
+    df1.orderBy(asc("PeaceScore"))
+      .show(20)   // Get the row with the lowest score
   }
 
   def getSummary(df: DataFrame): Unit = {
     val df1 = getScore(df)
-    df1.describe("Score")
+    df1.describe("PeaceScore")
       .show()
   }
 
   def getBadLocation(df: DataFrame, seuil: Int): Unit = {
     val df1 = getScore(df)
     // Duplique des lignes (à enlever pour plus tard)
-    val dup_df = df1.withColumn("new_col", explode(array((1 until 4).map(lit): _*))).selectExpr(df.columns: _*)
-    val df_sc = getScore(dup_df)
-    df_sc.where(col("Score") <= seuil)
+    //val dup_df = df1.withColumn("new_col", explode(array((1 until 4).map(lit): _*))).selectExpr(df.columns: _*)
+    val df_sc = getScore(df1)
+    df_sc.where(col("PeaceScore") <= seuil)
       .groupBy("location")
       .count()
       .sort(desc("count"))
